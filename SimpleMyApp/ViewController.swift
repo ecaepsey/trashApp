84324ddda671ba9a82515c5b2c7e8a6a4600c4ec
damir <yespeace@bk.ru>
2021-02-05 15:30:31 +0600

    Initial Commit

---
+++ SimpleMyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMyApp
+//
+//  Created by yespeace on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
